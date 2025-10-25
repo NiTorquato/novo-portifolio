2e3554d05fb296930247369acfa7acbd46ff7baf
Nicodemos Torquato <nicotor4@hotmail.com>
2025-10-25 19:00:04 -0300

    Update em informações

--- scripts/script.js
+++ scripts/script.js
@@ -87,7 +87,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
+// idade
+const anoNascimento = 2001;
+const mesNascimento = 11; // novembro
+const diaNascimento = 3;
+
+const hoje = new Date();
+let idade = hoje.getFullYear() - anoNascimento;
+
+const mesAtual = hoje.getMonth() + 1;
+const diaAtual = hoje.getDate();
+
+if (mesAtual < mesNascimento || (mesAtual === mesNascimento && diaAtual < diaNascimento)) {
+  idade--;
+}
+
+document.getElementById('idade').textContent = idade;
+
+
 // Inicialização
 document.addEventListener('DOMContentLoaded', () => {
     loadTheme();
 });
+
